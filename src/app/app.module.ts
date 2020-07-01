8be215d3cd4982c78b026b9fcd8e70fce64b5f0d
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2020-07-01 09:25:23 +0200

    Elimina core.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
+import { OverlayModule } from '@angular/cdk/overlay';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
 import { HeaderComponent } from './header/header.component';
 import { MovieModule } from './movie/movie.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { UserModule } from './user/user.module';
-import { OverlayModule } from '@angular/cdk/overlay';
 
 @NgModule({
   declarations: [
@@ -21,7 +20,6 @@ import { OverlayModule } from '@angular/cdk/overlay';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    CoreModule,
     MovieModule,
     UserModule,
     OverlayModule
