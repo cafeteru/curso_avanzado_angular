a46f676c9ff28a04bdcb277027f54a7d45cf0164
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2020-07-02 14:41:17 +0200

    Añade debounce events y ngOnDestroy

--- src/app/movie/movies/movie-search/movie-search.component.ts
+++ src/app/movie/movies/movie-search/movie-search.component.ts
@@ -3,13 +3,15 @@ import {
   ChangeDetectorRef,
   Component,
   EventEmitter,
+  OnDestroy,
   OnInit,
   Output
 } from '@angular/core';
-import {Observable} from 'rxjs';
+import {interval, Observable, ReplaySubject, Subject} from 'rxjs';
 import {Movie} from 'src/domain/movie';
 
 import {MoviesService} from '../../../shared/services/movies.service';
+import {debounce, takeWhile} from 'rxjs/operators';
 
 @Component({
   selector: 'app-movie-search',
@@ -17,29 +19,42 @@ import {MoviesService} from '../../../shared/services/movies.service';
   styleUrls: ['./movie-search.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MovieSearchComponent implements OnInit {
-
+export class MovieSearchComponent implements OnInit, OnDestroy {
   filteredMovies$: Observable<Movie[]>;
-
+  onFilterMovie$: Subject<string>;
   @Output()
   search: EventEmitter<string> = new EventEmitter();
+
+  private isComponentDestroyed: boolean = false;
 
   constructor(
     private moviesService: MoviesService,
     private changeDetectorRef: ChangeDetectorRef
   ) {
+    this.onFilterMovie$ = new ReplaySubject(1);
+  }
+
+  ngOnDestroy(): void {
+    this.isComponentDestroyed = true;
   }
 
   ngOnInit(): void {
+    this.onFilterMovie$
+      .pipe(
+        takeWhile(() => !this.isComponentDestroyed),
+        debounce(() => interval(1000)))
+      .subscribe((searchTerm) => {
+        this.filteredMovies$ = this.moviesService.search(searchTerm);
+        this.changeDetectorRef.detectChanges();
+      });
   }
 
-  filterMovies(searchTerm: string) {
-    this.filteredMovies$ = this.moviesService.search(searchTerm);
-    this.changeDetectorRef.detectChanges();
+  filterMovies(searchTerm: string): void {
+    this.onFilterMovie$.next(searchTerm);
   }
 
   onSubmit(form: any): void {
-    if (form.valid){
+    if (form.valid) {
       this.search.emit(form.value.searchTerm);
     }
   }
